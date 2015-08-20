828d45c0933a2fa1f3a44382d121ced827985a12
BertrandFritsch <bertrand.fritsch.in@gmail.com>
2015-08-20 10:54:26 +0200

    Main frame ready

--- Sources/WIMT/app.jsx
+++ Sources/WIMT/app.jsx
@@ -1,35 +1,127 @@
 ﻿/** @jsx React.DOM */
 
-var MainFrame = React.createClass({
+var AutoCompleteSelector = React.createClass({
+  getDefaultProps: function () {
+    return {
+      placeholder: '',
+      data: []
+    }
+  },
+
+  componentDidMount: function () {
+    var me = this;
+
+    $(this.getDOMNode()).kendoAutoComplete({
+      placeholder: this.props.placeholder,
+      dataSource: this.props.data,
+      filter: 'contains',
+      select: function (e) {
+        me.props.onStopChange(this.dataItem(e.item.index()));
+      }
+    });
+  },
+
   render: function () {
     return (
+      <input style={{width:100 + '%'}} />
+      )
+  }
+});
+
+var Trips = React.createClass({
+  render: function () {
+    var me = this,
+      stops = this.props.stops.map(function (stop) {
+      return stop.name;
+    });
+
+    function onStopChange(stopName, eventToCall) {
+      var index;
+
+      index = stops.indexOf(stopName);
+      //TODO: ensure the name has been found
+      me.props[eventToCall](me.props.stops[index]);
+    }
+
+    function onDepartureStopChange(stopName) {
+      onStopChange(stopName, 'onDepartureStopChange');
+    }
+
+    function onArrivalStopChange(stopName) {
+      onStopChange(stopName, 'onArrivalStopChange');
+    }
+
+    return (
         <div data-g-layout-container='"horizontalBubbling": false, "verticalBubbling": false' className="main-frame">
-          <input type="text"
-                 placeholder="De..."
-                 ref="from"
-                 data-g-layout-item='"row": 0' />
+          <div data-g-layout-item='"row": 0'>
+            <AutoCompleteSelector ref="from"
+                                  placeholder="De..."
+                                  data={stops}
+                                  onStopChange={onDepartureStopChange}
+                                   />
+          </div>
           <div data-g-layout-item='"row": 1, "isXSpacer": true, "isYSpacer": true' />
-          <input type="text"
-                 placeholder="Vers..."
-                 ref="to"
-                 data-g-layout-item='"row": 2' />
+          <div data-g-layout-item='"row": 2'>
+            <AutoCompleteSelector ref="to"
+                                  placeholder="Vers..."
+                                  data={stops}
+                                 onStopChange={onArrivalStopChange} />
+          </div>
         </div>
       )
   }
 });
 
 var Main = React.createClass({
+  getDefaultProps: function () {
+    return {
+      stops: (function () {
+        var stops = [], stop;
+
+        for (stop in Stops) {
+          stops.push(Stops[stop]);
+        }
+
+        return stops.sort(function (stop1, stop2) {
+          return stop1.name < stop2.name ? -1 : 1;
+        });
+      })()
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      departureStop: null,
+      arrivalStop: null
+    };
+  },
+
+  onDepartureStopChange: function (stop) {
+    this.setState({
+      departureStop: stop
+    });
+  },
+
+  onArrivalStopChange: function (stop) {
+    this.setState({
+      arrivalStop: stop
+    });
+  },
+
   render: function () {
     return (
-        <div id="gGridLayoutRoot" 
+        <div id="gGridLayoutRoot"
              className="gLayoutMeasuring"
-             data-g-layout-container=""
-        >
+             data-g-layout-container="">
           <div data-g-layout-item='"row": 0, "column": 0, "isXSpacer": true' />
           <div data-g-layout-item='"row": 0, "column": 1, "isYSpacer": true'
-               data-g-layout-policy='"widthPolicy": "Container", "widthHint": "600px"' 
+               data-g-layout-policy='"widthPolicy": "Container", "widthHint": "600px"'
                className="root-container">
-            <MainFrame />
+            <Trips stops={this.props.stops} 
+                   departureStop={this.state.departureStop} 
+                   arrivalStop={this.state.arrivalStop}
+                   onDepartureStopChange={this.onDepartureStopChange}
+                   onArrivalStopChange={this.onArrivalStopChange} />
           </div>
           <div data-g-layout-item='"row": 0, "column": 2, "isXSpacer": true' />
         </div>
