5cadf60f5b8b1bf360ff4bfe2da85d354730846b
BertrandFritsch <bertrand.fritsch.in@gmail.com>
2015-08-29 07:22:12 +0200

    10 first stops

--- Sources/WIMT/app.jsx
+++ Sources/WIMT/app.jsx
@@ -38,14 +38,166 @@ var AutoCompleteSelector = React.createClass({
     return (
       <input style={{width:100 + '%'}} />
       )
-  }
+}
+});
+
+var StopTimeRow = React.createClass({
+  render: function () {
+    var hours = Math.floor(this.props.stopTime.time / 60),
+      minutes = this.props.stopTime.time - (hours * 60),
+      lastStop;
+
+    if (hours >= 24) {
+      hours -= 24;
+    }
+
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    lastStop = this.props.stopTime.trip.getStopTimes()[this.props.stopTime.trip.getStopTimes().length - 1].stop.name;
+
+    return (
+      <div className="stop-time-row">{hours}:{minutes} - {this.props.stopTime.trip.mission} - {lastStop}</div>
+      )
+}
+});
+
+var DayHeaderRow = React.createClass({
+  render: function () {
+    return (
+      <div className="day-header-row">{this.props.date.toLocaleString('fr-FR', { weekday: 'long' })}</div>
+      )
+}
+});
+
+var SelectedTrips = React.createClass({
+  nextStopTimes: function() {
+    var me = this,
+      startStopTimes = me.props.startStopTimes,
+      currentStopTime = -1,
+      dayInMinutes = 24 * 60,
+      date = new Date(),
+      time,
+      stopTime, endStopTime, endSeq, d,
+      result = [];
+
+    function getDateAsString(date) {
+      var year = date.getFullYear(),
+        month = date.getMonth() + 1,
+        day = date.getDate();
+
+      if (month < 10) {
+        month = '0' + month;
+      }
+
+      if (day < 10) {
+        day = '0' + day;
+      }
+     
+      return day + '/' + month + '/' + year;
+    }
+
+    // generate the 10 first stop times, then return a function to get the next 10 ones
+    if (startStopTimes) {
+      // progress to the stop time 0
+      for (currentStopTime = 0; currentStopTime < startStopTimes.length; ++currentStopTime) {
+        time = startStopTimes[currentStopTime].time
+        if (time > dayInMinutes) {
+          time -= dayInMinutes;
+        }
+
+        if (time >= date.getHours() * 60 + date.getMinutes()) {
+          break;
+        }
+      }
+
+      // jump back to simulate the previous examined item
+      // it's all right even if we go at the -1 index, as the first action is to go forward
+      --currentStopTime;
+
+      while (true) {
+        ++currentStopTime;
+        if (currentStopTime === startStopTimes.length) {
+          currentStopTime = 0;
+          date.setDate(date.getDate() + 1);
+        }
+
+        stopTime = startStopTimes[currentStopTime];
+
+        if (me.props.departureStop && me.props.arrivalStop) {
+          // filter end stops trips
+          endStopTime = stopTime.trip.getStopTimes().firstOrDefault(function (stopTime) {
+            return stopTime.stop.id === me.props.arrivalStop.id;
+          });
+
+          endSeq = endStopTime ? endStopTime.sequence : 0;
+
+          if (stopTime.sequence >= endSeq) {
+            continue;
+          }
+        }
+
+        d = date;
+        // be aware of trips that starts the day before
+        if (stopTime.time > dayInMinutes) {
+          d.setDate(d.getDate() - 1);
+        }
+
+        var doesRunAt = stopTime.trip.getServiceExceptions()[getDateAsString(d)];
+        var endDate = stopTime.trip.service && stopTime.trip.service.endDate;
+        if (endDate) {
+          endDate.setDate(endDate.getDate() + 1);
+        }
+
+        if (doesRunAt === true || (doesRunAt === undefined && stopTime.trip.service !== null && stopTime.trip.service.startDate.getTime() <= d.getTime() && d < endDate && stopTime.trip.service.days[d.getDay()]))
+        {
+          result.push({ date: date, stopTime: stopTime });
+        }
+
+        if (result.length === 10) {
+          return result;
+        }
+      }
+    }
+
+    return result;
+  },
+
+  render: function () {
+    var me = this,
+      stopTimes, length, i,
+      rows = [],
+      date;
+
+    stopTimes = me.nextStopTimes();
+    length = stopTimes.length;
+
+    for (i = 0; i < length; ++i) {
+      if (date !== stopTimes[i].date) {
+        date = stopTimes[i].date;
+        rows.push(<DayHeaderRow key={date} date={date} />);
+      }
+
+      rows.push(<StopTimeRow key={i} stopTime={stopTimes[i].stopTime} />)
+    }
+
+    return (
+      <div>{rows}</div>
+      )
+}
 });
 
 var Trips = React.createClass({
   render: function () {
     var me = this,
         departureStops = me.props.stops,
-        arrivalStops;
+        arrivalStops,
+        startStopTimes;
 
     function onStopChange(stop, eventToCall) {
       var index;
@@ -84,6 +236,8 @@ var Trips = React.createClass({
           return stopsMap[stop.id] !== undefined;
         });
       })();
+
+      startStopTimes = me.props.departureStop.getStopTimes();
     }
     else {
       arrivalStops = departureStops;
@@ -98,7 +252,11 @@ var Trips = React.createClass({
                                   value={this.props.departureStop}
                                   onStopChange={onDepartureStopChange} />
           </div>
-          <div data-g-layout-item='"row": 1, "isXSpacer": true, "isYSpacer": true' />
+          <div className="trips-container" data-g-layout-item='"row": 1, "isXSpacer": true, "isYSpacer": true'>
+            <SelectedTrips departureStop={this.props.departureStop} 
+                           startStopTimes={startStopTimes}
+                           arrivalStop={this.props.arrivalStop} />
+          </div>
           <div data-g-layout-item='"row": 2'>
             <AutoCompleteSelector ref="to"
                                   placeholder="Vers..."
