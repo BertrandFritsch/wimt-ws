23d6a07277bc8dd7dd34cd723800b3f3801c8103
BertrandFritsch <bertrand.fritsch.in@gmail.com>
2015-08-31 08:48:56 +0200

    Introduce gulp tasks

---
+++ Sources/WIMT/gulpfile.js
@@ -0,0 +1,10 @@
+﻿// including plugins
+var gulp = require('gulp'),
+  uglify = require("gulp-uglify");
+
+// task
+gulp.task('minify-js', function () {
+  gulp.src('./SNCFData/*.js') // path to your files
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist'));
+});
