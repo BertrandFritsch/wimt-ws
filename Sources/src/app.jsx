923db4bfd4c5cc3bb6715108a82534c960ad6e17
Bertrand Fritsch <bertrand.fritsch.in@gmail.com>
2015-11-09 10:32:39 +0100

    Reintroduce babel-polyfill to have the support of Array.find on IE 11

--- Sources/src/app.jsx
+++ Sources/src/app.jsx
@@ -5,6 +5,7 @@ import ExtendedElement from './extendedElement';
 import ExtendedString from './extendedString';
 import Main from './components/Main/Main';
 import GridLayout from './gridlayout/gridlayout';
+import BabelPolyfill from 'babel-polyfill';
 
 ReactDOM.render(<Main />, document.getElementById('main-container'));
 GridLayout.initialize();
