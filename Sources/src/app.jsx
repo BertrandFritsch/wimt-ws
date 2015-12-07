6c9edd156e0bff6813d5165389b2cb75f172cf81
Bertrand Fritsch <bertrand.fritsch.in@gmail.com>
2015-12-07 18:17:13 +0100

    Remove custom string extensions in favor of ES6 features

--- Sources/src/app.jsx
+++ Sources/src/app.jsx
@@ -2,7 +2,6 @@
 import ReactDOM from 'react-dom';
 import ExtendedArray from './extendedArray';
 import ExtendedElement from './extendedElement';
-import ExtendedString from './extendedString';
 import Main from './components/Main/Main';
 import GridLayout from './gridlayout/gridlayout';
 import BabelPolyfill from 'babel-polyfill';
