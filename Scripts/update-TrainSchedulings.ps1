8e95f337ad5ad2b96ce256d1052ccacd7c889d9e
Bertrand Fritsch <bertrand.fritsch.in@gmail.com>
2018-05-10 12:28:25 +0200

    Fix path for MacOS

--- Scripts/update-TrainSchedulings.ps1
+++ Scripts/update-TrainSchedulings.ps1
@@ -1,52 +1,48 @@
 param(
-    [Parameter(Mandatory=$False)]
-    [switch] $Check = $false
+  [Parameter(Mandatory = $False)]
+  [switch] $Check = $false
 )
 
 $rootDir = $(
-    $dir = Convert-Path .
-    while (-not (Test-Path $dir\.git)) {
-        $dir = Split-Path $dir -Parent
-        if ($dir -eq '') {
-          throw "The root of the disk has been reached without finding the root project directory.`nPlease go to a directory of the project"
-        }
+  $dir = Convert-Path .
+  while (-not (Test-Path $dir\.git)) {
+    $dir = Split-Path $dir -Parent
+    if ($dir -eq '') {
+      throw "The root of the disk has been reached without finding the root project directory.`nPlease go to a directory of the project"
     }
-    $dir
+  }
+  $dir
 )
 
 $tmpFile = [IO.Path]::GetTempFileName()
-$outputDir = $( if ($Check ) { "$tmpFile.dir" } else { "$rootDir\Assets\gtfs-lines-last" } )
+$outputDir = $( if ($Check ) { "$tmpFile.dir" } else { "$rootDir/Assets/gtfs-lines-last" } )
 
 Write-Host "Get the remote assets"
 Invoke-RestMethod 'http://medias.sncf.com/sncfcom/open-data/gtfs/gtfs-lines-last.zip' -OutFile "$tmpFile.zip"
 
 Remove-Item $tmpFile
 
-if (-not $Check -and (Test-Path $outputDir))
-{
-    Write-Host "Remove previous asset files"
-    Remove-Item $outputDir -Recurse
+if (-not $Check -and (Test-Path $outputDir)) {
+  Write-Host "Remove previous asset files"
+  Remove-Item $outputDir -Recurse
 }
 
-7z x "-o$outputDir" "$tmpFile.zip" | Out-Null
+# 7z x "-o$outputDir" "$tmpFile.zip" | Out-Null
+unzip "$tmpFile.zip" -d $outputDir
 
-if ($Check)
-{
-    $outDatedFiles = Get-Item  $outputDir\* |? { -not(Test-Path "$rootDir\Assets\gtfs-lines-last\$($_.Name)") -or (Get-Item "$rootDir\Assets\gtfs-lines-last\$($_.Name)").LastWriteTime -lt $_.LastWriteTime }
-    if ($outDatedFiles )
-    {
-        Write-Host "Some assets are not up to date:"
-        $outDatedFiles |% { Write-Host "  - $($_.Name)" }
-    }
-    else
-    {
-        Write-Host "Assets are up to date!"
-    }
+if ($Check) {
+  $outDatedFiles = Get-Item $outputDir/* |? { -not(Test-Path "$rootDir/Assets/gtfs-lines-last/$($_.Name)") -or (Get-Item "$rootDir/Assets/gtfs-lines-last/$($_.Name)").LastWriteTime -lt $_.LastWriteTime }
+  if ($outDatedFiles ) {
+    Write-Host "Some assets are not up to date:"
+    $outDatedFiles | % { Write-Host "  - $($_.Name)" }
+  }
+  else {
+    Write-Host "Assets are up to date!"
+  }
 }
 
 Remove-Item "$tmpFile.zip"
 
-if ($Check)
-{
-    Remove-Item $outputDir -Recurse
+if ($Check) {
+  Remove-Item $outputDir -Recurse
 }
